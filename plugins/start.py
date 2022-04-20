9a4836ca20d25dd37e545f682f8576d0fc7fc2d7
ak-695 <100299703+ak-695@users.noreply.github.com>
2022-04-20 22:24:21 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -96,9 +96,7 @@ async def start_command(client: Client, message: Message):
                     InlineKeyboardButton("⚡️ Aʙᴏᴜᴛ Mᴇ", callback_data = "about"),
                     InlineKeyboardButton("Cʟᴏsᴇ 🔒", callback_data = "close")
                 ],
-                [
-                    InlineKeyboardButton("🖥️ Hᴏᴡ Tᴏ Oᴡɴ Tʜɪs Bᴏᴛ 🖥️", url="https://youtu.be/ep3u-n_DI_Q")
-                ]
+                             
             ]
         )
         await message.reply_text(
